abde91a26bf719645e8134cbed20958cbdb07bb7
sendyakbar <msendyakbars@gmail.com>
2018-11-22 15:36:00 +0700

    add login page, add dashboard page

--- routes/users/index.js
+++ routes/users/index.js
@@ -1,5 +1,10 @@
 const router = require('express').Router()
+const UserController = require('../../controllers/userController')
 
+
+router.get('/login', UserController.renderLoginPage)
+router.post('/login', UserController.postLogin)
+router.get('/dashboard', UserController.renderDashboard)
 
 
 module.exports = router
